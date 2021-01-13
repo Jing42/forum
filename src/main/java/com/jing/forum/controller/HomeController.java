267380758ec38249d5187db5c81463319310be7e
Jing42 <1445917015a@gmail.com>
2021-01-13 11:19:33 +0800

    page

--- src/main/java/com/jing/forum/controller/HomeController.java
+++ src/main/java/com/jing/forum/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.jing.forum.controller;
 
 import com.jing.forum.entity.DiscussPost;
+import com.jing.forum.entity.Page;
 import com.jing.forum.entity.User;
 import com.jing.forum.service.DisCussPostService;
 import com.jing.forum.service.UserService;
@@ -25,8 +26,13 @@ public class HomeController {
     private UserService userService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model){
-        List<DiscussPost> list = disCussPostService.findDiscussPosts(0, 0, 10);
+    public String getIndexPage(Model model, Page page){
+        // page由dispatcherServlet初始化，并自动装到model里
+        // SpringMVC自动实例化model和page，并注入model
+        page.setRows(disCussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = disCussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
         for (DiscussPost discussPost : list) {
             Map<String, Object> map = new HashMap<>();
---
+++ src/main/java/com/jing/forum/entity/Page.java
@@ -0,0 +1,83 @@
+package com.jing.forum.entity;
+
+
+public class Page {
+
+    private int current = 1;
+
+    private int limit = 10;
+
+    // 数据总数
+    private int rows;
+
+    // 查询路径
+    private String path;
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(int current) {
+        if(current >= 1)
+            this.current = current;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        if(limit >= 1 && limit <= 100)
+            this.limit = limit;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        if(rows >= 0)
+            this.rows = rows;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * 当前页起始行
+     * @return
+     */
+    public int getOffset() {
+        return (current - 1) * limit;
+    }
+
+
+    /**
+     * 获取总页数
+     * @return
+     */
+    public int getTotal() {
+        if(rows % limit == 0) return rows / limit;
+        else return rows / limit;
+    }
+
+    /**
+     * 获取起始页码
+     * @return
+     */
+    public int getFrom() {
+        int from = current - 2;
+        return from < 1 ? 1 : from;
+    }
+
+    public int getTo() {
+        int to = current + 2;
+        int total = getTotal();
+        return to > total ? total: to;
+    }
+}
