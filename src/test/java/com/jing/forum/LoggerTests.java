0f7f5cad242ad097ca923fd60744f43b97063710
Jing42 <1445917015a@gmail.com>
2021-01-13 14:17:33 +0800

    logger

---
+++ src/test/java/com/jing/forum/LoggerTests.java
@@ -0,0 +1,24 @@
+package com.jing.forum;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class LoggerTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger() {
+        System.out.println(logger.getName());
+
+        logger.debug("debuglog");
+        logger.info("infolog");
+        logger.warn("warnlog");
+        logger.error("error log");
+    }
+}
