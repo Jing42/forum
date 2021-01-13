ecc4924139d6f2911ef466bd570985252cd77cc7
Jing42 <1445917015a@gmail.com>
2021-01-13 17:46:14 +0800

    email

--- src/main/java/com/jing/forum/ForumApplication.java
+++ src/main/java/com/jing/forum/ForumApplication.java
@@ -3,6 +3,7 @@ package com.jing.forum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ForumApplication {
 
---
+++ src/test/java/com/jing/forum/MailTests.java
@@ -0,0 +1,25 @@
+package com.jing.forum;
+
+
+import com.jing.forum.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1445917015@qq.com", "TEsF", "WELCOME");
+    }
+}
