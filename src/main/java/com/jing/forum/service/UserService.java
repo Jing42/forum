424f9a806f3121092569278c5196e7c2df39db0f
Jing42 <1445917015a@gmail.com>
2021-01-14 16:58:52 +0800

    login

--- src/main/java/com/jing/forum/service/UserService.java
+++ src/main/java/com/jing/forum/service/UserService.java
@@ -2,6 +2,7 @@ package com.jing.forum.service;
 
 import com.jing.forum.dao.UserMapper;
 import com.jing.forum.entity.User;
+import com.jing.forum.util.ForumConstant;
 import com.jing.forum.util.ForumUtil;
 import com.jing.forum.util.MailClient;
 import org.apache.commons.lang3.StringUtils;
@@ -9,12 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements  ForumConstant{
     @Autowired
     private UserMapper userMapper;
 
@@ -40,34 +44,62 @@ public class UserService {
             throw new IllegalArgumentException("参数不能为空");
         }
         if(StringUtils.isBlank(user.getUsername())) {
-            map.put("usernameMessage", "账号不能为空!");
+            map.put("usernameMsg", "账号不能为空!");
             return map;
         }
         if(StringUtils.isBlank(user.getPassword())) {
-            map.put("passwordMessage", "密码不能为空!");
+            map.put("passwordMsg", "密码不能为空!");
             return map;
         }
         if(StringUtils.isBlank(user.getEmail())) {
-            map.put("emailMessage", "邮箱不能为空!");
+            map.put("emailMsg", "邮箱不能为空!");
             return map;
         }
 
         User user1 = userMapper.selectByName(user.getUsername());
         if(user1 != null) {
-            map.put("usernameMessage", "该账号已存在");
+            map.put("usernameMsg", "该账号已存在");
             return map;
         }
 
         User user2 = userMapper.selectByEmail(user.getEmail());
         if(user2 != null) {
-            map.put("emailMessage", "该邮箱已存在");
+            map.put("emailMsg", "该邮箱已存在");
             return map;
         }
 
         user.setSalt(ForumUtil.generateUUID().substring(0,5));
         user.setPassword(ForumUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(ForumUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
 
+
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+
+        String content = templateEngine.process("/mail/activation", context);
+
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
         return map;
+    }
+
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if(user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FALURE;
+        }
     }
 
 }
---
+++ src/main/java/com/jing/forum/util/ForumConstant.java
@@ -0,0 +1,10 @@
+package com.jing.forum.util;
+
+public interface ForumConstant {
+
+    public final static int ACTIVATION_SUCCESS = 0;
+
+    public final static int ACTIVATION_REPEAT = 1;
+
+    public final static int ACTIVATION_FALURE = 2;
+}
