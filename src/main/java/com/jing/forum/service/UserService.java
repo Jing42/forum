b04e41f614f47ab2dc1ac565ce7411beaa917db2
Jing42 <1445917015a@gmail.com>
2021-01-14 12:02:50 +0800

    register

--- src/main/java/com/jing/forum/service/UserService.java
+++ src/main/java/com/jing/forum/service/UserService.java
@@ -2,16 +2,57 @@ package com.jing.forum.service;
 
 import com.jing.forum.dao.UserMapper;
 import com.jing.forum.entity.User;
+import com.jing.forum.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${forum.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map= new HashMap<>();
+        if(user == null) {
+            throw new IllegalArgumentException("参数不能为空")；
+        }
+        if(StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMessage", "账号不能为空!");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMessage", "密码不能为空!");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMessage", "邮箱不能为空!");
+            return map;
+        }
+
+
+        return map;
     }
 
 }
---
+++ src/main/java/com/jing/forum/util/ForumUtil.java
@@ -0,0 +1,21 @@
+package com.jing.forum.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class ForumUtil {
+
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    public static String md5(String key) {
+        if(StringUtils.isBlank(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+}
