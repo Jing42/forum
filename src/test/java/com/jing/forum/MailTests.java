04caef0d9e8a2751a8751e6911b8a3dabc28eace
Jing42 <1445917015a@gmail.com>
2021-01-14 11:17:21 +0800

    register

--- src/test/java/com/jing/forum/MailTests.java
+++ src/test/java/com/jing/forum/MailTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 @ContextConfiguration(classes = ForumApplication.class)
@@ -22,4 +23,16 @@ public class MailTests {
     public void testTextMail() {
         mailClient.sendMail("1445917015@qq.com", "TEsF", "WELCOME");
     }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String process = templateEngine.process("/mail/demo", context);
+        System.out.println(process);
+
+        mailClient.sendMail("1445917015@qq.com", "hallo", process);
+    }
+
 }
